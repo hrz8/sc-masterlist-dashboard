15ed23c8854b0d38bbf7d26d8d1c6d1f608865f9
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2021-09-20 22:53:00 +0700

    add 404 page 😵‍💫

--- src/Routes.svelte
+++ src/Routes.svelte
@@ -6,11 +6,13 @@
   import Home from './pages/Home.svelte'
   import Partlist from './pages/Partlist.svelte'
   import PartnerList from './pages/Partner.svelte'
+  import Page404 from './pages/404.svelte'
 
   const routes = {
     '/': Home,
     '/part-list': Partlist,
-    '/partner-list': PartnerList
+    '/partner-list': PartnerList,
+    '/*': Page404
   }
 </script>
 
