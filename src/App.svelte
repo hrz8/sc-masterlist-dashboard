e6bd2e93069c5f068ca87925a33fb9ca2d0a69e5
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2021-09-20 23:24:37 +0700

    add sidebar ☠️

--- src/App.svelte
+++ src/App.svelte
@@ -4,22 +4,32 @@
   import Counter from './lib/Counter.svelte'
 
   import Router from './Routes.svelte'
+  import Sidebar from './components/shared/Sidebar.svelte'
 </script>
 
 <Styles />
 
 <main>
-  <h1>Hello Typescript!</h1>
-
-  <Counter />
+  <Sidebar />
+  <!-- <h1>Hello Typescript!</h1>
 
   <nav>
     <a use:link href="/">Home</a>
     <a use:link href="/part-list">Part List</a>
     <a use:link href="/partner-list">Partner List</a>
-  </nav>
-
-  <hr>
+  </nav> -->
 
   <Router />
 </main>
+
+<style>
+  main {
+    display: flex;
+    flex-wrap: nowrap;
+    height: 100vh;
+    height: -webkit-fill-available;
+    max-height: 100vh;
+    overflow-x: auto;
+    overflow-y: hidden;
+  }
+</style>
---
+++ src/components/shared/Sidebar.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+  import {
+    Icon,
+    Tooltip,
+    Dropdown,
+    DropdownItem,
+    DropdownMenu,
+    DropdownToggle
+  } from 'sveltestrap'
+  import { link } from 'svelte-spa-router'
+  import active from 'svelte-spa-router/active'
+</script>
+
+<div id="sidebar-wrapper" class="d-flex flex-column flex-shrink-0">
+  <a use:link href="/" id="sidebar-app" class="bg-dark bg-gradient text-light text-center p-3 link-dark text-decoration-none">
+    <Icon class="h3" name="house-door" />
+  </a>
+  <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
+    <li>
+      <a use:link use:active={{path: '/', className: 'active'}} href="/" id="sidebar-dashboard" class="nav-link py-3 border-bottom">
+        <Icon class="h4" name="speedometer2" />
+      </a>
+      <Tooltip target="sidebar-dashboard" placement="right">Dashboard</Tooltip>
+    </li>
+    <li>
+      <a use:link use:active={{path: '/part-list*', className: 'active'}} href="/part-list" id="sidebar-editor" class="nav-link py-3 border-bottom">
+        <Icon class="h4" name="bounding-box" />
+      </a>
+      <Tooltip target="sidebar-editor" placement="right">Part List</Tooltip>
+    </li>
+    <li>
+      <a use:link use:active={{path: '/partner-list*', className: 'active'}} href="/partner-list" id="sidebar-faq" class="nav-link py-3 border-bottom">
+        <Icon class="h4" name="people" />
+      </a>
+      <Tooltip target="sidebar-faq" placement="right">Partner List</Tooltip>
+    </li>
+  </ul>
+  <Dropdown class="border-top" direction="right" style="position: unset;">
+    <DropdownToggle tag="div">
+      <a href="/" on:click="{(e) => e.preventDefault()}" class="d-flex align-items-center justify-content-center p-3 link-dark text-decoration-none">
+        <Icon class="h4" name="person-circle" />
+      </a>
+    </DropdownToggle>
+    <DropdownMenu class="text-small shadow">
+      <DropdownItem header>Account</DropdownItem>
+      <DropdownItem>
+        <Icon class="h4" name="person-badge" /> Profile
+      </DropdownItem>
+      <DropdownItem>
+        <Icon class="h4" name="gear" /> Settings
+      </DropdownItem>
+      <DropdownItem divider />
+      <DropdownItem>
+        <Icon class="h4" name="box-arrow-right" /> Sign Out
+      </DropdownItem>
+    </DropdownMenu>
+  </Dropdown>
+</div>
+
+<style>
+#sidebar-wrapper {
+  width: 4.5em;
+  height: 100vh;
+  overflow-y: auto;
+  box-shadow: 0 0.125em 0.5em rgb(0 0 0 / 15%);
+}
+.nav-flush .nav-link {
+  border-radius: 0;
+}
+</style>
