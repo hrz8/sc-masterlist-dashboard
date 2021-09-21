521d3f82bf6b4f445246a896b2ca98e339efd450
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2021-09-21 12:08:27 +0700

    reduce sveltestrap usage 🧨

--- src/components/shared/Sidebar.svelte
+++ src/components/shared/Sidebar.svelte
@@ -1,60 +1,87 @@
 <script lang="ts">
-  import {
-    Icon,
-    Tooltip,
-    Dropdown,
-    DropdownItem,
-    DropdownMenu,
-    DropdownToggle
-  } from 'sveltestrap'
+  import { Icon, Tooltip } from 'sveltestrap'
   import { link } from 'svelte-spa-router'
   import active from 'svelte-spa-router/active'
 </script>
 
-<div id="sidebar-wrapper" class="d-flex flex-column flex-shrink-0">
-  <a use:link href="/" class="bg-dark bg-gradient text-light text-center p-3 link-dark text-decoration-none">
+<div
+  id="sidebar-wrapper"
+  class="d-flex flex-column flex-shrink-0"
+>
+  <a
+    use:link
+    href="/"
+    class="bg-dark bg-gradient text-light text-center p-3 link-dark text-decoration-none">
     <Icon class="h3" name="house-door" />
   </a>
-  <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
+  <ul
+    class="nav nav-pills nav-flush flex-column mb-auto text-center"
+  >
     <li>
-      <a use:link use:active={{path: '/', className: 'active'}} href="/" id="sidebar-dashboard" class="nav-link py-3 border-bottom">
+      <a
+        id="sidebar-dashboard"
+        use:link
+        use:active={{path: '/', className: 'active'}}
+        href="/"
+        class="nav-link py-3 border-bottom">
         <Icon class="h4" name="speedometer2" />
       </a>
       <Tooltip target="sidebar-dashboard" placement="right">Dashboard</Tooltip>
     </li>
     <li>
-      <a use:link use:active={{path: '/part-list*', className: 'active'}} href="/part-list" id="sidebar-editor" class="nav-link py-3 border-bottom">
+      <a
+        id="sidebar-partlist"
+        use:link
+        use:active={{path: '/part-list*', className: 'active'}}
+        href="/part-list"
+        class="nav-link py-3 border-bottom">
         <Icon class="h4" name="bounding-box" />
       </a>
-      <Tooltip target="sidebar-editor" placement="right">Part List</Tooltip>
+      <Tooltip target="sidebar-partlist" placement="right">Part List</Tooltip>
     </li>
     <li>
-      <a use:link use:active={{path: '/partner-list*', className: 'active'}} href="/partner-list" id="sidebar-faq" class="nav-link py-3 border-bottom">
+      <a
+        id="sidebar-partner-list"
+        use:link
+        use:active={{path: '/partner-list*', className: 'active'}}
+        href="/partner-list"  class="nav-link py-3 border-bottom">
         <Icon class="h4" name="people" />
       </a>
-      <Tooltip target="sidebar-faq" placement="right">Partner List</Tooltip>
+      <Tooltip target="sidebar-partner-list" placement="right">Partner List</Tooltip>
     </li>
   </ul>
-  <Dropdown class="border-top" direction="right" style="position: unset;">
-    <DropdownToggle tag="div">
-      <a href="/" on:click="{(e) => e.preventDefault()}" class="d-flex align-items-center justify-content-center p-3 link-dark text-decoration-none">
-        <Icon class="h4" name="person-circle" />
-      </a>
-    </DropdownToggle>
-    <DropdownMenu class="text-small shadow">
-      <DropdownItem header>Account</DropdownItem>
-      <DropdownItem>
-        <Icon class="h4" name="person-badge" /> Profile
-      </DropdownItem>
-      <DropdownItem>
-        <Icon class="h4" name="gear" /> Settings
-      </DropdownItem>
-      <DropdownItem divider />
-      <DropdownItem>
-        <Icon class="h4" name="box-arrow-right" /> Sign Out
-      </DropdownItem>
-    </DropdownMenu>
-  </Dropdown>
+  <div class="dropdown border-top" style="position: unset;">
+    <a
+      id="sidebar-dropdown-account"
+      on:click="{(e) => e.preventDefault()}"
+      href="/"
+      class="d-flex align-items-center justify-content-center p-3 link-dark text-decoration-none dropdown-toggle" 
+      data-bs-toggle="dropdown"
+      aria-expanded="false">
+      <i class="h4 bi-person-circle"></i>
+    </a>
+    <ul class="dropdown-menu text-small shadow" aria-labelledby="sidebar-dropdown-account">
+      <li>
+        <h6 class="dropdown-header">Account</h6>
+      </li>
+      <li>
+        <a use:link class="dropdown-item" href="/account/account">
+          <i class="h4 bi-person-badge"></i> Profile
+        </a>
+      </li>
+      <li>
+        <a use:link class="dropdown-item" href="/account/settings">
+          <i class="h4 bi-gear"></i> Settings
+        </a>
+      </li>
+      <li><hr class="dropdown-divider"></li>
+      <li>
+        <a use:link class="dropdown-item" href="/account/logout">
+          <i class="h4 bi-box-arrow-right"></i> Sign Out
+        </a>
+      </li>
+    </ul>
+  </div>
 </div>
 
 <style>
